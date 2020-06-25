bd587455741aab3fc1a444b3615831afe7dbd25a
DanZter <dannysimon11@gmail.com>
2020-06-25 23:29:43 +0100

    25th July

--- Intro_Building_Machine_Learning_Framework/src/run.sh
+++ Intro_Building_Machine_Learning_Framework/src/run.sh
@@ -2,6 +2,7 @@ export TRAINING_DATA = input/train_folds.csv
 export TEST_DATA = input/test.csv
 export MODEL = $1
 
+#hafsfasfasfasfasfasafello
 #FOLD=0 python -m src.train
 #FOLD=1 python -m src.train
 #FOLD=2 python -m src.train
